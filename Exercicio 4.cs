a042f441e0aefd09895497997a2ec99d9aa31669
Eric Almeida <37628478+almeieric@users.noreply.github.com>
2018-11-17 21:19:31 -0200

    Update Exercicio 4.cs

--- Exercicio 4.cs
+++ Exercicio 4.cs
@@ -1,47 +1,50 @@
-﻿using System;
+using System;
 
 namespace Exercicio_4
 {
     class Program
     {
-       
-        
+        static void Triangulo(int ladoBase, int ladoA, int ladoB)
+        {
+           
+
+                        
+
+            if (ladoBase == ladoA && ladoA == ladoB && ladoB == ladoBase)
+            {
+                Console.WriteLine("Equilatero");
+            } else if (ladoA == ladoB)
+            {
+                Console.WriteLine("Isoceles");
+            } else if (ladoBase != ladoA && ladoA != ladoB && ladoB != ladoBase)
+            {
+                Console.WriteLine("Escaleno");
+            }
+        }
+
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Insira Base: ");
             int a = Int32.Parse(Console.ReadLine());
-            int b = Int32.Parse(Console.ReadLine());
-            int c = Int32.Parse(Console.ReadLine());
-            Triangulo test = new Triangulo();
+            Console.WriteLine("Sua base é: " + a);
 
-            Console.WriteLine(test.ChecarTriangulo(a, b, c));
+
+            Console.WriteLine("Insira Lado A: ");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Sua lado A é: " + b);
+
+
+            Console.WriteLine("Insira Lado B: ");
+            int c = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Sua lado B: " + c);
+
+
+            Console.Write("\nSeu triangulo é: ");
+            Triangulo(a, b, c);
+
+
+            Console.WriteLine();
             Console.ReadKey();
         }
     }
-    class Triangulo
-    {
-        private int a;
-        private int baset;
-        private int b;
-        public Triangulo()
-        {
-            this.a = 0;
-            this.baset = 0;
-            this.b = 0;
-        }
-
-        public string ChecarTriangulo(int a, int baset, int b)
-        {
-            if (a == baset && baset == b && b == a)
-            {
-                return "é equilatero";
-            }else if(a == baset && a != b)
-            {
-                return "é isóseles";
-            } else if (a != baset && a!= b && b!= baset)
-            {
-                return "é equilatero";
-            }
-        }
-    }
-}
+}  

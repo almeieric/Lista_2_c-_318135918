d805b4104530ec6af07cb8529357980967604a0a
Eric Almeida <37628478+almeieric@users.noreply.github.com>
2018-11-17 22:04:51 -0200

    Add files via upload

--- Exercicio 5.cs
+++ Exercicio 5.cs
@@ -4,9 +4,39 @@ namespace Exercicio_5
 {
     class Program
     {
-        
+
+     
+
+        static void Letras(char letra)
+        {
+            if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' || letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U')
+            {
+                Console.WriteLine("é Vogal");
+            }
+            else
+            {
+                Console.WriteLine("é Consoante");
+            }
+        }
+
         static void Main(string[] args)
         {
+
+            char letra;
+            
+            
+                Console.WriteLine("Insira a letra");
+                letra = Convert.ToChar(Console.ReadLine());
+            
+
+
+            Console.Write("\nA letra: " + letra + " ");
+            Letras(letra);
+
+            Console.ReadKey();
+
+
+
            
         }
     }
